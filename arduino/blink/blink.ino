92b01388599c8d14685cdca14d9e270c1df2f90d
betich <betich365@gmail.com>
2024-05-07 17:00:45 +0700

    feat: add arduino code

---
+++ arduino/blink/blink.ino
@@ -0,0 +1,17 @@
+const int LED_BUILTIN = 2;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial.println("Hi");
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Update");
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(1000);
+}
